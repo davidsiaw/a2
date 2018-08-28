cebd33e59d598504403c39b9d101476d00f7caf9
David Siaw <davidsiaw@gmail.com>
2018-08-28 11:29:17 +0900

    add sdl ttf

--- src/a2.cpp
+++ src/a2.cpp
@@ -4,6 +4,8 @@
 
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
 typedef struct Sprite
 {
@@ -59,14 +61,73 @@ int main(int argc, char *argv[])
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return 1;
+	}
+
 	IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP);
+	Mix_Init(MIX_INIT_FLAC | MIX_INIT_MOD  | MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_OPUS | MIX_INIT_MID);
 
 	if (SDL_CreateWindowAndRenderer(1024, 768, 0, &window, &renderer) < 0)
+	{
 		exit(2);
+	}
 
-	Sprite sprite = LoadSprite("sample.png", renderer);
-	if (sprite.texture == NULL)
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1)
+	{
+		printf("Mix_OpenAudio: %s\n", Mix_GetError());
 		exit(2);
+	}
+
+	if (TTF_Init() == -1) {
+		printf("TTF_Init: %s\n", TTF_GetError());
+		exit(2);
+	}
+
+	// load font.ttf at size 16 into font
+	TTF_Font *font;
+	font = TTF_OpenFont("sample.ttf", 16);
+	if (!font) {
+		printf("TTF_OpenFont: %s\n", TTF_GetError());
+		// handle error
+	}
+
+	SDL_Color color = { 0,0,0 };
+	SDL_Surface *text_surface;
+
+
+	if (!(text_surface = TTF_RenderUTF8_Blended(font, "鼻血のハローdesu", color))) {
+		printf("TTF_RenderUNICODE_Solid: %s\n", TTF_GetError());
+	}
+	printf("%x\n", L'は');
+
+	Sprite t;
+	t.texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+	if (!t.texture) {
+		fprintf(stderr, "Couldn't create texture: %s\n", SDL_GetError());
+		SDL_FreeSurface(text_surface);
+	}
+	t.w = text_surface->w;
+	t.h = text_surface->h;
+	SDL_FreeSurface(text_surface);
+
+	Sprite sprite = LoadSprite("sample.webp", renderer);
+	if (sprite.texture == NULL)
+	{
+		exit(2);
+	}
+
+	Mix_Music *music;
+	music = Mix_LoadMUS("sample.mp3");
+	if (!music) {
+		printf("Mix_LoadMUS: %s\n", Mix_GetError());
+	}
+
+	if (Mix_PlayMusic(music, -1) == -1) {
+		printf("Mix_PlayMusic: %s\n", Mix_GetError());
+		// well, there's no music, but most games don't break without music...
+	}
 
 	/* Main render loop */
 	Uint8 done = 0;
@@ -89,6 +150,7 @@ int main(int argc, char *argv[])
 		SDL_RenderClear(renderer);
 
 		draw(window, renderer, sprite);
+		draw(window, renderer, t);
 
 		/* Update the screen! */
 		SDL_RenderPresent(renderer);
